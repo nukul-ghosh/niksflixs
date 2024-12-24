69e2c397714bfe5dd46cff696edc08f234d3e6a0
Nukul Ghosh <ghosh.nukul@gmail.com>
2024-12-24 19:22:26 +0530

    added axios and tanstack query for hero

--- src/components/API/api.js
+++ src/components/API/api.js
@@ -1,3 +1,21 @@
-export function getHero () {
+import axios from 'axios';
 
+const headers = {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjUwYjdkMjk2ZWI2MTA2NjEwN2YzOTMwYjk2ZjE3NSIsIm5iZiI6MTczMjg4NjI4MC40NDcyODY0LCJzdWIiOiI2NzI3YmRiODcyMGYwNDc2ZjYwZDkwYjQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.LpFK3dt465lTshpk8hB9wWnASAw5cNL1JODGxsxHElU'
+    };
+
+const getHero = async () => {
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+    
+    try {
+        const response = await axios.get(url, { headers });
+        console.log('Data:', response.data.results);
+        return response.data.results;
+    } catch (error) {
+        console.error('Error fetching data:', error.message); // Handle errors
+        return error;
+    }
 };
+
+
+export {getHero};
